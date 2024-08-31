692e0529ec08212e3c75f750126e6c43585103ca
MelnCat <melncatuwu@gmail.com>
2024-08-30 23:48:51 -0700

    fix

--- main.ts
+++ main.ts
@@ -2,17 +2,19 @@ import { homedir } from "node:os";
 import * as path from "node:path";
 import { uuidv7 } from "npm:uuidv7";
 
-const watcher = Deno.watchFs(`${homedir()}/Downloads/`, { recursive: false });
+const watcher = Deno.watchFs(`${homedir().replaceAll("\\", "/")}/Downloads/`, { recursive: false });
 
 console.log("Watching for downloads...");
 
 for await (const event of watcher) {
 	if (event.kind !== "create") continue;
 	const [filePath] = event.paths;
-	const info = Deno.lstatSync(filePath);
-	if (!info.isFile) continue;
 	setTimeout(() => {
-		Deno.renameSync(filePath, `${path.dirname(filePath)}/${uuidv7()}${path.extname(filePath)}`);
-		console.log(`Renamed ${path.basename(filePath)}!`)
-	}, 100);
+		try {
+			const info = Deno.lstatSync(filePath);
+			if (!info.isFile) return;
+			Deno.renameSync(filePath, `${path.dirname(filePath)}/${uuidv7()}${path.extname(filePath)}`);
+			console.log(`Renamed ${path.basename(filePath)}!`);
+		} catch {}
+	}, 500);
 }
